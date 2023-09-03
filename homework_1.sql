14dd0cdad2283e936eb2d10ec525ab950e88b8ab
Emre ÜNALDI <emree.unaldi@gmail.com>
2023-09-03 12:11:01 +0300

    Homework 1 Completed

---
+++ homework_1.sql
@@ -0,0 +1,27 @@
+--- film tablosunda bulunan title ve description sütunlarındaki verileri sıralayan SQL kodu:
+SELECT title, description 
+FROM film;
+
+--- film tablosunda bulunan tüm sütunlardaki verileri film uzunluğu (length) 60 dan büyük VE 75 ten küçük olma koşullarıyla sıralayan SQL kodu:.
+SELECT * 
+FROM film 
+WHERE length > 60 AND length < 75;
+
+SELECT * 
+FROM film 
+WHERE length BETWEEN 60 AND 75;
+
+--- film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99 VE replacement_cost 12.99 VEYA 28.99 olma koşullarıyla sıralayan SQL kodu:
+SELECT * 
+FROM film 
+WHERE (rental_rate = 0.99 AND replacement_cost = 12.99) OR replacement_cost = 28.99;
+
+--- customer tablosunda bulunan first_name sütunundaki değeri 'Mary' olan müşterinin last_name sütunundaki değeri getiren SQL kodu:
+SELECT last_name 
+FROM customer
+WHERE first_name = 'Mary';
+
+--- film tablosundaki uzunluğu(length) 50 ten büyük OLMAYIP aynı zamanda rental_rate değeri 2.99 veya 4.99 OLMAYAN verileri sıralayan SQL kodu:
+SELECT * 
+FROM film
+WHERE length <= 50 AND rental_rate NOT IN (2.99, 4.99);
